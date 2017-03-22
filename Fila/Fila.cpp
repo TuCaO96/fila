e7dc53115eae33525fb6fc4c3ddb86e72d24a9ad
Arthur Mendonça Ribeiro <arthurmr13@hotmail.com.br>
2017-03-22 13:55:10 -0300

    Define classe inicial

---
+++ Fila/Fila.cpp
@@ -0,0 +1,37 @@
+class Fila {
+
+private:
+
+	int CapMax; // Capacidade Máxima de elementos
+
+	int Inicio; //Inicio da Fila
+
+	int Fim; //Fim da Fila
+
+	float *Items; /*Os items/elementos da fila (que serão
+
+				  alocados dinamicamente)*/
+
+public:
+
+	Fila(int N); /*Construtor padrão, aloca dinamicamente
+
+				 N floats para o atributo Items, e seta os atributos de
+
+				 controle da fila */
+
+	~Fila(); /*Destrutor da fila, deve-se desalocar a
+
+			 memória que foi adquirida no construtor.*/
+
+	void inserir(float NovoItem); //enfileira um novo item
+
+	float retirar(); //desenfileira um item (retornando-o)
+
+	bool vazia(); //verifica se a fila está vazia
+
+	bool cheia(); //verifica se a fila está cheia
+
+	int tamanho();//retorna quantos itens existem na fila
+
+};
